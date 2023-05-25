090d61593b62f2e429f582b8dec3d50346754acc
Abhii <Abhii@localhost>
2023-05-24 22:44:31 -0600

    Time based memory allocation

--- backtrace.cpp
+++ backtrace.cpp
@@ -11,6 +11,18 @@
 #include <gperftools/malloc_hook.h>
 using namespace std;
 #define BT_BUF_SIZE 100
+#define ARRAY_SIZE 100
+
+using std::chrono::duration;
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+
+struct AllocRecord
+{
+  size_t size;
+  int64_t time;
+};
 
 static void RecordAlloc(const void *ptr, size_t bytes, int skip_count);
 static void RecordFree(const void *ptr);
@@ -19,6 +31,21 @@ static void MyDeleteHook(const void *ptr);
 
 size_t allocSize = 0;
 size_t *allocSizePtr = &allocSize;
+
+high_resolution_clock::time_point startTime;
+static size_t sizeArr[ARRAY_SIZE];
+static high_resolution_clock::time_point timeArr[ARRAY_SIZE];
+int sizeIndex = 0;
+int *sizeIndexPtr = &sizeIndex;
+int timeIndex = 0;
+int *timeIndexPtr = &timeIndex;
+
+void myfunc2(int iterations){
+  const char* x[iterations];
+  for(int i=0;i<iterations;i++){
+    x[i]="ABCDEFGHIJKLMNOPQRSTUVXYZ"+char(i);
+  }
+}
 
 void myfunc(int ncalls)
 {
@@ -46,10 +73,15 @@ void myfunc(int ncalls)
 
   if (ncalls > 1)
     myfunc(ncalls - 1);
+
+  // for(int i=0;i<ncalls;i++){
+  //   myfunc2(i*10+1);
+  // }
 }
 
 static void MyNewHook(const void *ptr, size_t size)
 {
+  // printf("in my new hook %zu\n",size);
   if (ptr != NULL)
     RecordAlloc(ptr, size, 0);
 }
@@ -59,20 +91,6 @@ static void MyDeleteHook(const void *ptr)
   if (ptr != NULL)
     RecordFree(ptr);
 };
-
-int main(int argc, char *argv[])
-{
-
-  MallocHook::AddNewHook(&MyNewHook);
-  if (argc != 2)
-  {
-    fprintf(stderr, "%s num-calls %ld size\n", argv[0], allocSize);
-    exit(EXIT_FAILURE);
-  }
-
-  myfunc(atoi(argv[1]));
-  exit(EXIT_SUCCESS);
-}
 
 // Record an allocation in the profile.
 static void RecordAlloc(const void *ptr, size_t bytes, int skip_count)
@@ -87,6 +105,10 @@ static void RecordAlloc(const void *ptr, size_t bytes, int skip_count)
   // }
   // printf("record alloc %ld num-calls\n", bytes);
   // printf(allocStr, bytes);
+  sizeArr[*sizeIndexPtr] = bytes;
+  timeArr[*timeIndexPtr] =  high_resolution_clock::now();
+  *sizeIndexPtr += 1;
+  *timeIndexPtr += 1;
   *allocSizePtr += bytes;
 }
 
@@ -98,4 +120,31 @@ static void RecordFree(const void *ptr)
   //   heap_profile->RecordFree(ptr);
   //   MaybeDumpProfileLocked();
   // }
+}
+
+int main(int argc, char *argv[])
+{
+
+  MallocHook::AddNewHook(&MyNewHook);
+  if (argc != 2)
+  {
+    fprintf(stderr, "%s num-calls %ld size\n", argv[0], allocSize);
+    exit(EXIT_FAILURE);
+  }
+
+  myfunc(atoi(argv[1]));
+
+  std::cout << "finish running" << std::endl;
+  std::cout << "Allocations:" <<std::endl;
+  
+  for (int i = 0; i < *sizeIndexPtr && i<ARRAY_SIZE; i++)
+  {
+    if(sizeArr[i]>0)
+      std::cout << "symbol: "
+              << "size: " << sizeArr[i] << " - time: "
+              << duration_cast<milliseconds>(timeArr[i] - startTime).count() << std::endl;
+  }
+
+
+  exit(EXIT_SUCCESS);
 }
